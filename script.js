e95372536c55036498be3e012ee3443db0448257
Hamza Aarab <haarab@c1r2p6.1337.ma>
2025-01-07 21:59:01 +0100

    update

--- script.js
+++ script.js
@@ -5,143 +5,144 @@ const HomeComponent = () => {
 	<p>This is the home page of our vanilla JavaScript website.</p>
 	`;
 };
+
 const TournamentComponent = () => {
 	return `
-	<section class="Tournament" id="TOURNAMENT">
-			
-			<div class="TOURNAMENT_list">Tournament<br>List
-				<div class="table">
-					<div class="table-row">
-						<table>
-							<thead class="col1">
-								<tr>
-									<th scope="col" class="col-tournaments">Tournaments<br>Competitors</th>
-									<th scope="col" class="col-matches">Matches<br>Won</th>
-									<th scope="col" class="col-tournaments-won">Tournaments<br>Won</th>
-									<th scope="col" class="col-success-rate">Success Rate</th>
-									<th scope="col" class="col-points-earned">Points<br>Earned</th>
-								</tr>
-								</tr>
-							</thead>
-							<tbody>
-								<tr>
-									<th scope="row" class="col-tournaments">
-										<div class="row-content">
-											<img src="/imgs/Screen Shot 2024-10-02 at 2.04.41 AM.png" alt="Midnight Sons Image">
-											<div class="text-content">
-												Midnight Sons<br>
-												<span class="level">LvL 15</span>
-											</div>
+	<link rel="stylesheet" href="tournament.css">
+	<section class="Tournament" id="TOURNAMENT">	
+		<div class="TOURNAMENT_list">Tournament<br>List
+			<div class="table">
+				<div class="table-row">
+					<table>
+						<thead class="col1">
+							<tr>
+								<th scope="col" class="col-tournaments">Tournaments<br>Competitors</th>
+								<th scope="col" class="col-matches">Matches<br>Won</th>
+								<th scope="col" class="col-tournaments-won">Tournaments<br>Won</th>
+								<th scope="col" class="col-success-rate">Success Rate</th>
+								<th scope="col" class="col-points-earned">Points<br>Earned</th>
+							</tr>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<th scope="row" class="col-tournaments">
+									<div class="row-content">
+										<img src="/imgs/Screen Shot 2024-10-02 at 2.04.41 AM.png" alt="Midnight Sons Image">
+										<div class="text-content">
+											Midnight Sons<br>
+											<span class="level">LvL 15</span>
 										</div>
-									</th>
-									<td class="col-matches">Today,<br>12:59 pm</td>
-									<td class="col-tournaments-won">Today,<br>12:59 pm</td>
-									<td class="col-success-rate">Today,<br>12:59 pm</td>
-									<td class="col-points-earned">Today,<br>12:59 pm</td>
-								</tr>
-								<tr>
-									<th scope="row" class="col-tournaments">
-										<div class="row-content">
-											<img src="/imgs/Screen Shot 2024-10-02 at 2.04.41 AM.png" alt="Midnight Sons Image">
-											<div class="text-content">
-												Midnight Sons<br>
-												<span class="level">LvL 15</span>
-											</div>
+									</div>
+								</th>
+								<td class="col-matches">Today,<br>12:59 pm</td>
+								<td class="col-tournaments-won">Today,<br>12:59 pm</td>
+								<td class="col-success-rate">Today,<br>12:59 pm</td>
+								<td class="col-points-earned">Today,<br>12:59 pm</td>
+							</tr>
+							<tr>
+								<th scope="row" class="col-tournaments">
+									<div class="row-content">
+										<img src="/imgs/Screen Shot 2024-10-02 at 2.04.41 AM.png" alt="Midnight Sons Image">
+										<div class="text-content">
+											Midnight Sons<br>
+											<span class="level">LvL 15</span>
 										</div>
-									</th>
-									<td class="col-matches">Today,<br>12:59 pm</td>
-									<td class="col-tournaments-won">Today,<br>12:59 pm</td>
-									<td class="col-success-rate">Today,<br>12:59 pm</td>
-									<td class="col-points-earned">Today,<br>12:59 pm</td>
-								</tr>
-								<tr>
-									<th scope="row" class="col-tournaments">
-										<div class="row-content">
-											<img src="/imgs/Screen Shot 2024-10-02 at 2.04.41 AM.png" alt="Midnight Sons Image">
-											<div class="text-content">
-												Midnight Sons<br>
-												<span class="level">LvL 15</span>
-											</div>
+									</div>
+								</th>
+								<td class="col-matches">Today,<br>12:59 pm</td>
+								<td class="col-tournaments-won">Today,<br>12:59 pm</td>
+								<td class="col-success-rate">Today,<br>12:59 pm</td>
+								<td class="col-points-earned">Today,<br>12:59 pm</td>
+							</tr>
+							<tr>
+								<th scope="row" class="col-tournaments">
+									<div class="row-content">
+										<img src="/imgs/Screen Shot 2024-10-02 at 2.04.41 AM.png" alt="Midnight Sons Image">
+										<div class="text-content">
+											Midnight Sons<br>
+											<span class="level">LvL 15</span>
 										</div>
-									</th>
-									<td class="col-matches">Today,<br>12:59 pm</td>
-									<td class="col-tournaments-won">Today,<br>12:59 pm</td>
-									<td class="col-success-rate">Today,<br>12:59 pm</td>
-									<td class="col-points-earned">Today,<br>12:59 pm</td>
-								</tr>
-								<tr>
-									<th scope="row" class="col-tournaments">
-										<div class="row-content">
-											<img src="/imgs/Screen Shot 2024-10-02 at 2.04.41 AM.png" alt="Midnight Sons Image">
-											<div class="text-content">
-												Midnight Sons<br>
-												<span class="level">LvL 15</span>
-											</div>
+									</div>
+								</th>
+								<td class="col-matches">Today,<br>12:59 pm</td>
+								<td class="col-tournaments-won">Today,<br>12:59 pm</td>
+								<td class="col-success-rate">Today,<br>12:59 pm</td>
+								<td class="col-points-earned">Today,<br>12:59 pm</td>
+							</tr>
+							<tr>
+								<th scope="row" class="col-tournaments">
+									<div class="row-content">
+										<img src="/imgs/Screen Shot 2024-10-02 at 2.04.41 AM.png" alt="Midnight Sons Image">
+										<div class="text-content">
+											Midnight Sons<br>
+											<span class="level">LvL 15</span>
 										</div>
-									</th>
-									<td class="col-matches">Today,<br>12:59 pm</td>
-									<td class="col-tournaments-won">Today,<br>12:59 pm</td>
-									<td class="col-success-rate">Today,<br>12:59 pm</td>
-									<td class="col-points-earned">Today,<br>12:59 pm</td>
-								</tr>
-							</tbody>
-						</table>
+									</div>
+								</th>
+								<td class="col-matches">Today,<br>12:59 pm</td>
+								<td class="col-tournaments-won">Today,<br>12:59 pm</td>
+								<td class="col-success-rate">Today,<br>12:59 pm</td>
+								<td class="col-points-earned">Today,<br>12:59 pm</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+
+			<!-- <div class="Tournament_details"> -->
+				
+				
+		<div class="TOURNAMENT_details">Tournament<br>Details
+			<div class="Matches">
+				<div class="TournamentFirst">Tournament First Matches :</div>
+					<div class="tableMatches">
+
+						<div class="vsMatch">
+							<div class="tournament-content">
+								<img src="/imgs/Screen Shot 2024-10-02 at 2.04.41 AM.png" alt="Midnight Sons Image">
+								<div class="tournament-matches">
+									Midnight Sons<br>
+									<span class="level">LvL 15</span>
+								</div>
+							</div>
+							<div class="vs-text">vs</div>
+							<div class="tournament-content">
+								<img src="/imgs/Screen Shot 2024-10-02 at 2.04.41 AM.png" alt="Midnight Sons Image">
+								<div class="tournament-matches">
+									Midnight Sons<br>
+									<span class="level">LvL 15</span>
+								</div>
+							</div>
+						</div>
+						<div class="vsMatch">
+							<div class="tournament-content">
+								<img src="/imgs/Screen Shot 2024-10-02 at 2.04.41 AM.png" alt="Midnight Sons Image">
+								<div class="tournament-matches">
+									Midnight Sons<br>
+									<span class="level">LvL 15</span>
+								</div>
+							</div>
+							<div class="vs-text">vs</div>
+							<div class="tournament-content">
+								<img src="/imgs/Screen Shot 2024-10-02 at 2.04.41 AM.png" alt="Midnight Sons Image">
+								<div class="tournament-matches">
+									Midnight Sons<br>
+									<span class="level">LvL 15</span>
+								</div>
+							</div>
+						</div>
 					</div>
-				</div>
+				<div class="TournamentPrizes">Tournament Prizes :</div>
+					<div class="prizes">
+						<div class="TournamentChampion">Tournament Champion : 1500 pts</div>
+						<div class="PongClub">Pong Club Reward : 500 pts</div>
+						<div class="TournamentParticipation">Tournament Participation : 300 pts</div>
+					</div>
 			</div>
-
-				<!-- <div class="Tournament_details"> -->
-					
-					
-			<div class="TOURNAMENT_details">Tournament<br>Details
-				<div class="Matches">
-					<div class="TournamentFirst">Tournament First Matches :</div>
-						<div class="tableMatches">
-
-							<div class="vsMatch">
-								<div class="tournament-content">
-									<img src="/imgs/Screen Shot 2024-10-02 at 2.04.41 AM.png" alt="Midnight Sons Image">
-									<div class="tournament-matches">
-										Midnight Sons<br>
-										<span class="level">LvL 15</span>
-									</div>
-								</div>
-								<div class="vs-text">vs</div>
-								<div class="tournament-content">
-									<img src="/imgs/Screen Shot 2024-10-02 at 2.04.41 AM.png" alt="Midnight Sons Image">
-									<div class="tournament-matches">
-										Midnight Sons<br>
-										<span class="level">LvL 15</span>
-									</div>
-								</div>
-							</div>
-							<div class="vsMatch">
-								<div class="tournament-content">
-									<img src="/imgs/Screen Shot 2024-10-02 at 2.04.41 AM.png" alt="Midnight Sons Image">
-									<div class="tournament-matches">
-										Midnight Sons<br>
-										<span class="level">LvL 15</span>
-									</div>
-								</div>
-								<div class="vs-text">vs</div>
-								<div class="tournament-content">
-									<img src="/imgs/Screen Shot 2024-10-02 at 2.04.41 AM.png" alt="Midnight Sons Image">
-									<div class="tournament-matches">
-										Midnight Sons<br>
-										<span class="level">LvL 15</span>
-									</div>
-								</div>
-							</div>
-						</div>
-					<div class="TournamentPrizes">Tournament Prizes :</div>
-						<div class="prizes">
-							<div class="TournamentChampion">Tournament Champion : 1500 pts</div>
-							<div class="PongClub">Pong Club Reward : 500 pts</div>
-							<div class="TournamentParticipation">Tournament Participation : 300 pts</div>
-						</div>
-				</div>
-			</div>
-		</section>
+		</div>
+	</section>
 	`;
 };
   
@@ -155,8 +156,8 @@ document.getElementById('app').innerHTML = com();
   
 // Navigation
 document.getElementById('index-link').addEventListener('click', () => render(HomeComponent));
-document.getElementById('tournaments').addEventListener('click', () => render(TournamentComponent));
-document.getElementById('TournamentTree-link').addEventListener('click', () => render(HomeComponent));
+document.getElementById('tournaments').addEventListener('click', () =>  render(TournamentComponent));
+// document.getElementById('TournamentTree-link').addEventListener('click', () => render(HomeComponent));
 
   
   // Initial render
