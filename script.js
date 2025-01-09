42780be9c38bbbaca0d7541f3d365bbdeaa2eeec
Hamza Aarab <haarab@c1r2p4.1337.ma>
2025-01-09 16:19:54 +0100

    tournament

--- script.js
+++ script.js
@@ -6,6 +6,136 @@ const HomeComponent = () => {
 	`;
 };
 
+
+const TournamentTree_Component = () => {
+	return `
+	<link rel="stylesheet" href="./TOURNAMENTS/Tournament.css">
+	<section class="Tournament" id="TOURNAMENT">	
+		<div class="TOURNAMENT_list">Tournament<br>Tree
+			<div class="Champion">
+				<div class="ChampionTouranament">
+					<div class="verticalLine"></div>
+					<div class="Championfinal">
+						<img src="/imgs/Screen Shot 2024-10-02 at 4.18.58 AM.png" alt="Midnight">
+					</div>
+					<div class="verticalLine"></div>
+				</div>
+				<!-- <div class="vs1"></div> -->
+				<div class="line">
+					<div class="FinalMatch">
+						<div class="Championfinal">
+							<img src="/imgs/Screen Shot 2024-10-02 at 4.18.58 AM.png" alt="Midnight">
+						</div>
+						<div class="Championfinal">
+							<img src="/imgs/Screen Shot 2024-10-02 at 4.35.56 AM.png" alt="Midnight">
+						</div>
+					</div>
+				</div>
+
+				<div class="Player">
+					<div class="Players">
+						<div class="Championfinal">
+							<img src="/imgs/Screen Shot 2024-10-02 at 4.18.58 AM.png" alt="Midnight">
+						</div>
+						<div class="vs"></div>
+						<div class="Championfinal">
+							<img src="/imgs/Screen Shot 2024-10-02 at 4.35.39 AM.png" alt="Midnight">
+						</div>
+					</div>
+					<div class="Players">
+						<div class="Championfinal">
+							<img src="/imgs/Screen Shot 2024-10-02 at 4.35.56 AM.png" alt="Midnight">
+						</div>
+						<div class="vs"></div>
+						<div class="Championfinal">
+							<img src="/imgs/Screen Shot 2024-10-02 at 4.36.27 AM.png" alt="Midnight">
+						</div>
+					</div>
+				</div>
+				
+				
+
+			</div>
+		</div>
+
+			<!-- <div class="Tournament_details"> -->
+				
+				
+		<div class="TOURNAMENT_details">Tournament<br>Details
+			<div class="Matches">
+				<div class="TournamentFirst">Tournament First Matches Result :</div>
+				<div class="tableMatches">
+					<div class="vsMatch">
+						<div class="tournament-content">
+							<img src="/imgs/Screen Shot 2024-10-02 at 2.04.41 AM.png" alt="Midnight Sons Image">
+							<div class="tournament-matches">
+								Midnight Sons<br>
+								<span class="level">LvL 15</span>
+							</div>
+						</div>
+						<div class="vs-text">5 - 3</div>
+						<div class="tournament-content">
+							<img src="/imgs/Screen Shot 2024-10-02 at 2.04.41 AM.png" alt="Midnight Sons Image">
+							<div class="tournament-matches">
+								Midnight Sons<br>
+								<span class="level">LvL 15</span>
+							</div>
+						</div>
+					</div>
+					<div class="vsMatch">
+						<div class="tournament-content">
+							<img src="/imgs/Screen Shot 2024-10-02 at 2.04.41 AM.png" alt="Midnight Sons Image">
+							<div class="tournament-matches">
+								Midnight Sons<br>
+								<span class="level">LvL 15</span>
+							</div>
+						</div>
+						<div class="vs-text">5 - 2</div>
+						<div class="tournament-content">
+							<img src="/imgs/Screen Shot 2024-10-02 at 2.04.41 AM.png" alt="Midnight Sons Image">
+							<div class="tournament-matches">
+								Midnight Sons<br>
+								<span class="level">LvL 15</span>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="TournamentFinal">Tournament Final Matche Result :</div>
+					<div class="tableMatches">
+						<div class="vsMatch">
+							<div class="tournament-content">
+								<img src="/imgs/Screen Shot 2024-10-02 at 2.04.41 AM.png" alt="Midnight Sons Image">
+								<div class="tournament-matches">
+									Midnight Sons<br>
+									<span class="level">LvL 15</span>
+								</div>
+							</div>
+							<div class="vs-text">1 - 5</div>
+							<div class="tournament-content">
+								<img src="/imgs/Screen Shot 2024-10-02 at 2.04.41 AM.png" alt="Midnight Sons Image">
+								<div class="tournament-matches">
+									Midnight Sons<br>
+									<span class="level">LvL 15</span>
+								</div>
+							</div>
+						</div>
+					
+					</div>
+				<div class="TournamentPrizes">Tournament Prizes :</div>
+					<div class="prizes">
+						<div class="TournamentChampion">Tournament Champion : 1500 pts</div>
+						<div class="PongClub">Pong Club Reward : 500 pts</div>
+						<div class="TournamentParticipation">Tournament Participation : 300 pts</div>
+					</div>
+			</div>
+		</div>
+		</div>
+	</section>
+
+	<script src="./Tournament.js"></script>
+	`;
+};
+
 const TournamentComponent = () => {
 	return `
 	<link rel="stylesheet" href="tournament.css">
@@ -157,7 +287,7 @@ document.getElementById('app').innerHTML = com();
 // Navigation
 document.getElementById('index-link').addEventListener('click', () => render(HomeComponent));
 document.getElementById('tournaments').addEventListener('click', () =>  render(TournamentComponent));
-// document.getElementById('TournamentTree-link').addEventListener('click', () => render(HomeComponent));
+document.getElementById('TournamentTree').addEventListener('click', () => render(TournamentTree_Component));
 
   
   // Initial render
