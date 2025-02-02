a2a6e07a3bdc93f49f8984ecd8b5dbc4f1019350
thee-falcon <omar.makran.10@gmail.com>
2025-02-02 02:07:45 +0100

    main js

---
+++ frontend/src/index.js
@@ -0,0 +1,8 @@
+import Login from "./pages/auth/Login.js";
+
+const root = document.getElementById('root');
+// change content of the di and  setup te repeation of the components
+
+root.innerHTML = `<login-page></login-page>`;
+
+console.log('Hello from index.js');
