0833f31d58c0a19bb95cf2e13e2acd75afac6b9f
thee-falcon <omar.makran.10@gmail.com>
2025-02-02 02:14:23 +0100

    add login and register urls

---
+++ backend/auth/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+urlpatterns = [
+    path("login", TokenObtainPairView.as_view(), name="login"),
+    path("register", views.register, name="register")
+]
