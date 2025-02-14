1f98e87794e0d34d40c212c883a86b686f0eddaf
Mohamed charaf Bouderraoui <mbouderr@c2r3p6.1337.ma>
2025-02-14 18:12:12 +0100

    homepage added

---
+++ front/static/js/home.js
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HOME</title>
+    <link rel="stylesheet" href="home.css">
+</head>
+<body>
+    <div id="ok">
+        <div class="card-container">
+
+        <div class="col1">
+            <div class="card1">
+                <!-- <img src="pictures/Screen_Shot_2025-01-12_at_12.47.51_AM-removebg-preview.png" alt="Game Image"> -->
+                <div class="card-content">
+                <a href="#" class="card-button">Start Game</a>
+                </div>
+            </div>
+            <div class="card4">
+                <!-- <img src="pictures/Screen Shot 2024-10-02 at 3.53.12 AM.png" alt="Training Image"> -->
+                <div class="card-content">
+                    <a href="#" class="card-button">Start Training</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col2">
+            <div class="card2">
+                <!-- <img src="pictures/Screen Shot 2024-10-02 at 3.44.26 AM.png" alt="Tournament Image"> -->
+                <div class="card-content">
+                    <a href="#" class="card-button">Join Tournament</a>
+                </div>
+            </div>
+            <div class="card5">
+                <!-- <img src="pictures/Screen Shot 2024-10-02 at 3.57.08 AM.png" alt="Leaderboard Image"> -->
+                <div class="card-content">
+                    <a href="#" class="card-button">Show Leaderboard</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="col3">
+            <div class="card3">
+                <div id="ko">
+                    <div id="lim">
+
+                    </div>
+                </div>
+                <div class="card-content">
+                    <a href="#" class="card-button">View Profile</a>
+                </div>
+            </div>
+            <div class="card6">
+                <!-- <img src="../image/Screen Shot 2024-10-02 at 4.18.58 AM.png" alt="Club Image"> -->
+                <div class="card-content">
+                    <a href="#" class="card-button">Join Club</a>
+                </div>
+            </div>
+        </div>
+        </div>
+    </div>
+</body>
+</html>
--- front/static/js/logged.js
+++ front/static/js/logged.js
@@ -6,15 +6,59 @@ class dashboard extends HTMLElement {
         
             console.log("profilePicture  " , profilePicture );
             this.innerHTML = `
-            <div>
-                <img 
-                    src=${profilePicture} 
-                     style="width: 100px; height: 100px; border-radius: 50%;"
-                >
-            </div>
-            <button id="logout">Logout</button> 
-              <h2>Welcome, ${username }!</h2> 
-            <p>You are logged in!</p>
+ 
+                <div class="card-container">
+        
+                <div class="col1">
+                    <div class="card1">
+                        <!-- <img src="../image/Screen_Shot_2025-01-12_at_12.47.51_AM-removebg-preview.png" alt="Game Image"> -->
+                        <div class="card-content">
+                        <a href="#" class="card-button">Start Game</a>
+                        </div>
+                    </div>
+                    <div class="card4">
+                        <!-- <img src="../image/Screen Shot 2024-10-02 at 3.53.12 AM.png" alt="Training Image"> -->
+                        <div class="card-content">
+                            <a href="#" class="card-button">Start Training</a>
+                        </div>
+                    </div>
+                </div>
+        
+                <div class="col2">
+                    <div class="card2">
+                        <!-- <img src="../image/Screen Shot 2024-10-02 at 3.44.26 AM.png" alt="Tournament Image"> -->
+                        <div class="card-content">
+                            <a href="#" class="card-button">Join Tournament</a>
+                        </div>
+                    </div>
+                    <div class="card5">
+                        <!-- <img src="../image/Screen Shot 2024-10-02 at 3.57.08 AM.png" alt="Leaderboard Image"> -->
+                        <div class="card-content">
+                            <a href="#" class="card-button">Show Leaderboard</a>
+                        </div>
+                    </div>
+                </div>
+        
+                <div class="col3">
+                    <div class="card3">
+                        <div id="ko">
+                            <div id="lim">
+        
+                            </div>
+                        </div>
+                        <div class="card-content">
+                            <a href="#" class="card-button">View Profile</a>
+                        </div>
+                    </div>
+                    <div class="card6">
+                        <!-- <img src="../image/Screen Shot 2024-10-02 at 4.18.58 AM.png" alt="Club Image"> -->
+                        <div class="card-content">
+                            <a href="#" class="card-button">Join Club</a>
+                        </div>
+                    </div>
+                </div>
+                </div>
+ 
         `;
    
 
