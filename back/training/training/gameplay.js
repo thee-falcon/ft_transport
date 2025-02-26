310bcff987447f7c7b0e73c6aa6ad7b7a4435a50
Nabil Bouhali <nbouhali@c1r2p7.1337.ma>
2025-02-26 07:46:00 +0000

    tfidakchi

--- back/training/training/gameplay.js
+++
@@ -1,9 +0,0 @@
-function check_colision(posy) {
-    const paddleTop = posy - paddle_height / 2;
-    const paddleBottom = posy + paddle_height / 2;
-    if (bally >= paddleTop && bally <= paddleBottom) {
-        return true;
-    }
-    return false;
-}
-
