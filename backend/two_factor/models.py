21ecf3cc80b2b260ed12cb6da23eb64adb0ca89f
thee-falcon <omar.makran.10@gmail.com>
2025-02-26 22:40:20 +0000

    create modles of verificationCode

--- backend/two_factor/models.py
+++ backend/two_factor/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.utils import timezone
 
-# Create your models here.
+User = get_user_model()
+
+class VerificationCode(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    code = models.CharField(max_length=6)
+    created_at = models.DateTimeField(auto_now_add=True)
+    expires_at = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.user.email} - {self.code}"
