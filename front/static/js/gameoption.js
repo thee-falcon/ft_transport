38e6ad9192c47e781623b38d4f2a708f5f70254a
Mohamed charaf Bouderraoui <mbouderr@c2r2p6.1337.ma>
2025-02-27 22:46:11 +0000

    HULK, SMASH

--- front/static/js/gameoption.js
+++ front/static/js/gameoption.js
@@ -9,19 +9,19 @@ this.innerHTML = `
 <body>
     
     <div class="container">
-        <div class="glass"  style="--r:-15;" >
+        <div class="glass" >
             <div class="Game_Modes_img">
                 <img src="./media/ping-pong.png" alt="">
             </div>
         <button class="button">Ping Pong</button>
         </div>
-        <div class="glass" style="--r:5;" >
+        <div class="glass"  >
             <div class="Game_Modes_img">
                 <img src="./media/people.png" alt="">
             </div>
           <button class="button">Multiplayer</button>
         </div>
-        <div class="glass" style="--r:25;" >
+        <div class="glass"  >
             <div class="Game_Modes_img">
                 <img src="./media/processor.png" alt="">
             </div>
